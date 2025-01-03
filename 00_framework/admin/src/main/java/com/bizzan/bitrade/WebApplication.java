dde60045274fc389bccd8de643576a2c65190651
horus <xiaoshewan@126.com>
2025-01-03 11:10:47 +0800

    [feat]upgrade eureka to nacos

--- 00_framework/admin/src/main/java/com/bizzan/bitrade/WebApplication.java
+++ 00_framework/admin/src/main/java/com/bizzan/bitrade/WebApplication.java
@@ -3,14 +3,12 @@ package com.bizzan.bitrade;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * @author Shaoxianjun
  * @create 2020年12月07日
  */
 @SpringBootApplication
-//@EnableAspectJAutoProxy(exposeProxy=true)
 @EnableDiscoveryClient
 public class WebApplication {
     public static void main(String[] args) {
